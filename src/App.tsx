7eb2afac83502c36e4c9777745f4b975d0d791dc
gmoinbong <vladosos.scherba@gmail.com>
2023-05-26 20:28:58 +0300

    build a Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
+import NavBar from './components/NavBar'
 function App() {
   return (
-    <Button colorScheme='blue'>Button</Button>
+    <Grid
+      templateAreas={`"header header"
+                  "nav main"
+                  "nav footer"`}
+      gridTemplateRows={'50px 1fr 30px'}
+      gridTemplateColumns={'150px 1fr'}
+      h='200px'
+      gap='1'
+      color='blackAlpha.700'
+      fontWeight='bold'
+    >
+      <GridItem pl='2' area={'header'}>
+        <NavBar />
+      </GridItem>
+      <GridItem pl='2' bg='pink.300' area={'nav'}>
+        Nav
+      </GridItem>
+      <GridItem pl='2' bg='green.300' area={'main'}>
+        Main
+      </GridItem>
+      <GridItem pl='2' bg='blue.300' area={'footer'}>
+        Footer
+      </GridItem>
+    </Grid>
   )
 }
 
