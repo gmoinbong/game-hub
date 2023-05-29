ded2c19e3692a262bb2bdcf21710dac64814bd44
gmoinbong <vladosos.scherba@gmail.com>
2023-05-29 12:26:43 +0300

    custom hook for fetch games

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 import NavBar from './components/NavBar'
+import GameGrid from './components/GameGrid'
 function App() {
   return (
     <Grid
-      templateAreas={`"header header"
-                  "nav main"
-                  "nav footer"`}
-      gridTemplateRows={'50px 1fr 30px'}
-      gridTemplateColumns={'150px 1fr'}
-      h='200px'
-      gap='1'
-      color='blackAlpha.700'
-      fontWeight='bold'
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: '1fr',
+        lg: '250px 1fr'
+      }}
     >
-      <GridItem pl='2' area={'header'}>
+      <GridItem area='nav'>
         <NavBar />
       </GridItem>
-      <GridItem pl='2' bg='pink.300' area={'nav'}>
-        Nav
+      <GridItem area="aside" paddingX={5}>        Main
       </GridItem>
-      <GridItem pl='2' bg='green.300' area={'main'}>
-        Main
-      </GridItem>
-      <GridItem pl='2' bg='blue.300' area={'footer'}>
+      <GridItem area="main">
         Footer
       </GridItem>
     </Grid>
