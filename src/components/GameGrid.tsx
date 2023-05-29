b9d3146d04c0007532892d3d4ccc7b602b17e829
gmoinbong <vladosos.scherba@gmail.com>
2023-05-29 16:33:09 +0300

    Generic hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,7 +7,7 @@ import GameCardContainer from './GameCardContainer';
 
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames()
+  const { data, error, isLoading } = useGames()
   const skeletons = [1, 2, 3, 4, 5, 6]
   return (
     <>
@@ -15,9 +15,9 @@ const GameGrid = () => {
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding={10} spacing={10}>
         {isLoading && skeletons.map((skeleton) => <GameCardContainer> <GameCardSkeleton key={skeleton} /></GameCardContainer>)}
-        {games.map((game) =>
+        {data.map((date) =>
           <GameCardContainer>
-            <GameCard key={game.id} game={game} />
+            <GameCard key={date.id} game={date} />
           </GameCardContainer>
         )}
       </SimpleGrid>
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,39 +1,7 @@
-import { CanceledError } from 'axios';
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
+import { useData } from './useData';
 
-export const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false)
-
-  interface Genre {
-    id: number;
-    name: string
-  }
-
-  interface GenresResponse {
-    count: number;
-    results: Genre[];
-
-  }
-
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true)
-    apiClient
-      .get<GenresResponse>("/genres", { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results)
-        setLoading(false)
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message)
-        setLoading(false)
-      });
-
-    return () => controller.abort();
-  }, []);
-  return { genres, error, isLoading }
+export interface Genre {
+  id: number;
+  name: string
 }
+export const useGenres = () => useData<Genre>('/genres') 
